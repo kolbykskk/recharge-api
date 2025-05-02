eedf45b528afb198e5f2c9139946403462335228
kolbykskk <kolbykalafut@gmail.com>
2025-05-02 18:06:11 -0500

    Add purchase item id and purchase item type

--- lib/recharge/classes.rb
+++ lib/recharge/classes.rb
@@ -145,7 +145,9 @@ class2 "Recharge", JSON.parse(<<-END) do
         "sku": "MILK-1",
         "title": "Sumatra Coffee",
         "variant_title": "Milk - a / b",
-        "total_price": "10.95"
+        "total_price": "10.95",
+        "purchase_item_id": 63898947,
+        "purchase_item_type": "subscription"
       }
     ],
     "note": "next order #1",
